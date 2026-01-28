031d5a265c0842252023a118e48372513092e81f
krunch <75719839+krunchiekrunch@users.noreply.github.com>
2026-01-28 17:37:52 +0000

    changed layout to numpad, added encoder support, disabled oled

--- firmware/main.py
+++ firmware/main.py
@@ -7,8 +7,10 @@ from kmk.extensions.display import Display, TextEntry
 from kmk.extensions.display.ssd1306 import SSD1306
 from kmk.extensions.RGB import RGB, AnimationModes
 from kmk.extensions.LED import LED
+from kmk.modules.encoder import EncoderHandler
 
 keyboard = KMKKeyboard()
+keyboard.modules.append(encoder_handler)
 
 # matrix
 keyboard.row_pins = (board.D6, board.D2, board.D1, board.D3)
@@ -17,15 +19,24 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     [
-        KC.Q, KC.W, KC.E, KC.R, KC.T,
-        KC.A, KC.S, KC.D, KC.F, KC.G,
-        KC.Z, KC.X, KC.C, KC.V, KC.B,
-        KC.LCTRL, KC.GUI, KC.LALT, KC.SPACE, KC.RCTRL,
+        KC.P0, KC.P1, KC.P4, KC.P7, KC.KC.NLCK,
+        KC.P0, KC.P2, KC.P5, KC.P8, KC.PSLS,
+        KC.PDOT, KC.P3, KC.P6, KC.P9, KC.PAST,
+        KC.PENT, KC.PENT, KC.PPLS, KC.PPLS, KC.PMNS,
     ]
 ]
 
-# 128x32 OLED display
+# volume knob
+'''
+encoder_handler.pins = ((board.D5, board.D4),)
+encoder_handler.map = [
+    ((KC.MW_DN, KC.MW_UP),), # Mouse wheel zooming
+    ((KC.VOLD, KC.VOLU),),   # Volume control
+    ]
+'''
 
+# 128x32 OLED display
+'''
 keyboard.SCL = board.D5
 keyboard.SDA = board.D4
 
@@ -40,6 +51,7 @@ krunchpad 20
     height=32,
 )
 keyboard.extensions.append(display)
+'''
 
 if __name__ == '__main__':
     keyboard.go()
